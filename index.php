6a1f245f64e4cfb78ac382a98451424b6b9f6cd6
Verma <varun.verma@sap.com>
2019-01-28 23:11:19 +0530

    Fixed JS warning

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 
 <body>
 
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	
     <!-- Bootstrap Core JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
