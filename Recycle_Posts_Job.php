dc25de2a8b46e31ec1df701c82439fac0ff937f4
Verma <varun.verma@sap.com>
2019-03-06 14:56:55 +0530

    Recycle post logic finalized

--- Recycle_Posts_Job.php
+++ Recycle_Posts_Job.php
@@ -1,5 +1,5 @@
 <!--
-	Copyright 2012  Varun Verma  (email : varunverma@varunverma.org)
+    Copyright 2012  Varun Verma  (email : varunverma@varunverma.org)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
@@ -18,15 +18,114 @@
 
 require( 'jokes_utility_functions.php' );
 
-$post_tag = $_POST['post_tag'];
-if($post_tag == null || $post_tag == ""){
-	die("Post Tag no supplied");
+createDBConnection();
+
+$result = array();
+
+if(check_recycle_required()){
+
+    // Get an very old post for recyling.
+    $post_tag = get_tag_for_recycling();
+
+    if($post_tag == ""){
+
+        $result = array("status_code" => "Success",
+                    "message" => "No tags found that can be recycled");
+
+    }
+    else{
+
+        $result = recycle_old_post($post_tag,0);
+    }   
+
+}
+else{
+    $result = array("status_code" => "Success",
+                    "message" => "Recycling not required");
 }
 
-createDBConnection();
-	
-// Get an very old post for recyling.
-$result = recycle_old_post($post_tag);
 echo json_encode($result);
+    
+// Functions
+function check_recycle_required(){
+
+    global $wpdb;
+    global $db;
+
+    $post_table_name = $wpdb->prefix.'posts';
+
+    // Get Max Post Date
+    $query = "SELECT max(post_date) as latest_post_date FROM $post_table_name WHERE post_status = 'publish' AND post_type = 'post'";
+    $stmt = $db->prepare($query);
+    $sqlVars = array();
+
+    if (!$stmt->execute($sqlVars)){
+        return false;
+    }
+    else{
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $latest_post_date = new DateTime($row['latest_post_date']); 
+        }
+
+        $now = new DateTime();  //Current Time
+        
+        $time_diff = $now->getTimestamp() - $latest_post_date->getTimestamp();
+
+        // If time diff is more than 8 hours
+        if($time_diff > 8*60*60){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    return false;
+}
+
+function get_tag_for_recycling(){
+
+    global $wpdb;
+    global $db;
+
+    $terms_table = $wpdb->prefix.'terms';
+    $taxonomy_table = $wpdb->prefix.'term_taxonomy';
+
+    $query = "SELECT terms.name, taxonomy.taxonomy, taxonomy.count FROM $terms_table as terms 
+                inner join $taxonomy_table as taxonomy on terms.term_id = taxonomy.term_id 
+                where taxonomy.taxonomy = 'post_tag' order by taxonomy.count desc limit 2";
+
+    $stmt = $db->prepare($query);
+    $sqlVars = array();
+    
+    if (!$stmt->execute($sqlVars)){
+        return "";
+    }
+    else{
+
+        $tag_list = array();
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $tag_list[] = $row['name'];
+        }
+
+        if(empty($tag_list)){
+            return "";
+        }
+        else{
+
+            $now = date("H");
+            if(count($tag_list) > 1 && $now > 12){
+                return $tag_list[1];
+            }
+            else{
+                return $tag_list[0];
+            }
+
+        }
+
+    }
+
+}
 
 ?>
